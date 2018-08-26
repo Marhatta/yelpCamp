7a1b642d369aebe4bb0c6d413bb075cb16797281
vishal marhatta <vishal.marhatta@gmail.com>
2018-08-26 18:08:28 +0000

    added mongolab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes       =require("./routes/comments"),
     indexRoutes         =require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp"); 
+//mongoose.connect("mongodb://localhost/yelp_camp"); 
+mongoose.connect("mongodb://vishal:Vishal%401996@ds133202.mlab.com:33202/yelpcamp_vishal"); 
+//@ in password is replaced by %40 because mongo oes not support @ in password
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
